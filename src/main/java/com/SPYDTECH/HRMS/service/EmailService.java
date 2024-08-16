77e7be834d5e922d2548bad9a01168ed637289f4
Pavan Kumar Velagana <126136343+pa1velagana@users.noreply.github.com>
2024-08-16 16:28:29 +0530

    Added email Subject in EmailService

--- src/main/java/com/SPYDTECH/HRMS/service/EmailService.java
+++ src/main/java/com/SPYDTECH/HRMS/service/EmailService.java
@@ -44,20 +44,18 @@ public class EmailService {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(toEmail);
-        helper.setSubject("Sending Username And Password");
-        //  helper.setText("Your username for registration is: " + userName);
-        helper.setText("Dear Canidate "+employee.getFirstName()+" "+employee.getLastName()+",\n" +
+        helper.setSubject("Welcome! Your HR Portal Access Has Been Set Up");
+        helper.setText("Dear " + employee.getFirstName() + " " + employee.getLastName() + ",\n" +
                 "We’re excited to inform you that your access to the HR Portal has been successfully set up. You can now log in to manage your personal information, benefits, and more.\n" +
-                "Your Login Credentials:\n" +
-                "Username:  " +toEmail+"\n"+
-                "Temporary Password: "+password+"\n" +
-                "\n" +
-                "How to Log In:\n" +
+                "\nYour Login Credentials:\n" +
+                "Username: " + toEmail + "\n" +
+                "Temporary Password: " + password + "\n" +
+                "\nHow to Log In:\n" +
                 "Visit the HR Portal: http://13.234.49.187:5173/\n" +
                 "Enter Your Username and Temporary Password: Use the credentials listed above.\n" +
                 "Change Your Password: After logging in, you will be prompted to change your temporary password. Please create a new, secure password that you’ll remember.\n" +
                 "Update Your Profile: Once logged in, please review and update your profile information as needed.\n" +
-                "If you have any questions or run into any issues, don’t hesitate to reach out to our IT support team at it-hr@spydtech.com. ");
+                "If you have any questions or run into any issues, don’t hesitate to reach out to our HR team at mounika@spydtech.com.");
 
         mailSender.send(message);
 
